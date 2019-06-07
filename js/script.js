26cbf6e2058884c76e3ca41bd4dbd95b4e652d78
brian <brayomumo>
2019-06-07 18:19:36 +0300

    style contact form

--- js/script.js
+++ js/script.js
@@ -12,11 +12,11 @@ $(document).ready (function(){
         $("#management p").toggle();
         $("#management img").toggle();
     })
-    function myFunction(){
-       var name = document.getElementById("name");
-       var email = document.getElementById("email");
+    // function myFunction(){
+    //    var name = document.getElementById("name");
+    //    var email = document.getElementById("email");
 
-       var msg = document.querySelector()
+    //    var msg = document.querySelector()
 
-    };
+    // };
 })
